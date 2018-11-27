6b625b2187436642fe7cbddc17eae1449a487321
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2018-11-27 23:02:35 +0900

    Set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return <div className="App" />;
+  public render() {
+    return <div className="App">hi</div>;
   }
 }
 

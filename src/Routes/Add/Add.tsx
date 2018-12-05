9c8f216d10bddb664e6906e4eba501e3f94a1677
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2018-12-05 22:01:47 +0900

    Add Note Route

--- src/Routes/Add/Add.tsx
+++ src/Routes/Add/Add.tsx
@@ -1,8 +1,50 @@
+import gql from "graphql-tag";
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import { RouteComponentProps } from "react-router-dom";
 import Editor from "../../Components/Editor";
 
-export default class Add extends Component {
+const ADD_NOTE = gql`
+  mutation createNote($title: String!, $content: String!) @client {
+    createNote(title: $title, content: $content) {
+      id
+    }
+  }
+`;
+
+interface IaddNote {
+  id: number;
+}
+
+interface IaddNoteVariables {
+  title: string;
+  content: string;
+  id: number;
+}
+
+interface IProps extends RouteComponentProps<any> {}
+
+class AddNoteMutation extends Mutation<IaddNote, IaddNoteVariables> {}
+
+export default class Add extends Component<IProps> {
+  private createNote;
+  public onSave = (title: string, content: string) => {
+    const {
+      history: { push }
+    } = this.props;
+    if (title !== "" && content !== "") {
+      this.createNote({ variables: { title, content } });
+      push("/");
+    }
+  };
   public render() {
-    return <Editor onChange={null} onSave={null} />;
+    return (
+      <AddNoteMutation mutation={ADD_NOTE}>
+        {createNote => {
+          this.createNote = createNote;
+          return <Editor onChange={null} onSave={this.onSave} />;
+        }}
+      </AddNoteMutation>
+    );
   }
 }

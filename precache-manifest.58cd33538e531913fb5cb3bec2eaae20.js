5a68a48f312eef803f7e923b79968c439c7d3e09
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2018-12-09 21:21:48 +0900

    Updates

--- precache-manifest.58cd33538e531913fb5cb3bec2eaae20.js
+++ precache-manifest.58cd33538e531913fb5cb3bec2eaae20.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/offline-notes/static/js/runtime~main.abbc5786.js"
   },
   {
-    "revision": "818fd02ab07a1c2a021e",
-    "url": "/offline-notes/static/js/main.818fd02a.chunk.js"
+    "revision": "3a8e7cf726994eaf9b21",
+    "url": "/offline-notes/static/js/main.3a8e7cf7.chunk.js"
   },
   {
     "revision": "8e0d6a956a559ba068c0",
     "url": "/offline-notes/static/js/1.8e0d6a95.chunk.js"
   },
   {
-    "revision": "34a9298dce5597fe58d93463b2eef6e2",
+    "revision": "b242f550728acb3ba77f35f1dbbe9b0c",
     "url": "/offline-notes/index.html"
   }
 ];

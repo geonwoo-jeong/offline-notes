1ff191801e505967aa4d6ced33d8ad12d97926de
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2018-12-09 21:33:35 +0900

    Component -> SFC

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,39 +1,19 @@
-import React, { Component } from "react";
+import React, { SFC } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Add from "../../Routes/Add";
 import Edit from "../../Routes/Edit";
 import Note from "../../Routes/Note";
 import Notes from "../../Routes/Notes";
 
-class App extends Component {
-  public render() {
-    return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route
-            exact={true}
-            path={process.env.PUBLIC_URL + "/"}
-            component={Notes}
-          />
-          <Route
-            exact={true}
-            path={process.env.PUBLIC_URL + "/add"}
-            component={Add}
-          />
-          <Route
-            exact={true}
-            path={process.env.PUBLIC_URL + "/edit/:id"}
-            component={Edit}
-          />
-          <Route
-            exact={true}
-            path={process.env.PUBLIC_URL + "/note/:id"}
-            component={Note}
-          />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const App: SFC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact={true} path={"/"} component={Notes} />
+      <Route path={"/add"} component={Add} />
+      <Route path={"/edit/:id"} component={Edit} />
+      <Route path={"/note/:id"} component={Note} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
